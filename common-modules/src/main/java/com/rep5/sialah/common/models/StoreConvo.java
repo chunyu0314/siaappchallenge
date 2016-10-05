2a053d0b0cc8fdfa69cf148a762222e738b042d2
lhhong <heok.hong.low@gmail.com>
2016-10-05 21:33:02 +0800

    changed convo

--- common-modules/src/main/java/com/rep5/sialah/common/models/StoreConvo.java
+++ common-modules/src/main/java/com/rep5/sialah/common/models/StoreConvo.java
@@ -5,14 +5,33 @@ package com.rep5.sialah.common.models;
  */
 public class StoreConvo {
 
+    private String sender;
+    private String message;
+
+    public StoreConvo() {
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
     //stores previous 10 convo for customer service, jx to set this value
     //store from earliest to latest
     //clear after 1st contact to reduce payload
     //prepend each string with source of msg, use "usr-Whatever message" and "bot-Whatever message"
-    private String[] lastTenConvo;
-
-    public StoreConvo() {
-    }
+    /*private String[] lastTenConvo;
 
     public String[] getLastTenConvo() {
         return lastTenConvo;
@@ -21,4 +40,5 @@ public class StoreConvo {
     public void setLastTenConvo(String[] lastTenConvo) {
         this.lastTenConvo = lastTenConvo;
     }
+    */
 }
